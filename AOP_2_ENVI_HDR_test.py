a4de5af1d91d58c23b19fcfe1b816515d05c0962
creeder23 <carlmreeder@gmail.com>
2016-07-19 11:31:33 -0600

    Test script for AOP_ENVI_HDR task

---
+++ AOP_2_ENVI_HDR_test.py
@@ -0,0 +1,22 @@
+import os
+
+def imafunction():
+    from gbdxtools import Interface
+    gbdx = Interface()
+    status = None
+    aop2envi = gbdx.Task("AOP_ENVI_HDR")
+    aop2envi.inputs.image = 's3://gbd-customer-data/7d8cfdb6-13ee-4a2a-bf7e-0aff4795d927/ENVI/SI/AOP/055026839010_01/'
+    
+    workflow = gbdx.Workflow([ aop2envi ])
+    workflow.savedata(
+       aop2envi.outputs.output_data,
+       location='Doctest/hdr/test2'
+    )
+    workflow.execute()
+    status = workflow.status 
+    return status
+    	
+print os.getcwd()
+
+if __name__ == "__main__":
+    imafunction()
