6eda5345ed6a55583dabc684c0d08d46e39b778c
Dan Getman <dan.getman@digitalglobe.com>
2016-07-19 09:18:42 -0600

    initial commit

---
+++ doc_process.py
@@ -0,0 +1,27 @@
+
+list_of_tasks = ['task_name']
+
+
+
+# Get a list of all task names
+#
+# For each task name
+#
+# run the doctext
+#
+# check the results
+#
+# If good
+#
+# determine of the md files exists
+#
+# if not create the md file from the template
+#
+# Open the md file
+#
+# run the api data collect
+#
+# populate the md file from the API
+#
+# populate the md file from the code
+
