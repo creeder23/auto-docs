9592a4c5313d85b2464053eab48925adeed42a74
Dan Getman <dan.getman@digitalglobe.com>
2016-07-19 10:23:36 -0600

    another commit

---
+++ test_test.py
@@ -0,0 +1,16 @@
+import os
+
+def imafunction():
+
+    result = None
+
+    result = 1+1
+
+    print result
+    print os.getcwd()
+
+    return result
+
+
+if __name__ == "__main__":
+    imafunction()
