e812019c42970b799e1dd3231ac440cda4d13965
creeder23 <creeder@DGCarlReeder.local>
2016-10-04 11:27:10 -0600

    change to image path

--- ENVI_ColorSliceClassification_qs.py
+++ ENVI_ColorSliceClassification_qs.py
@@ -3,20 +3,20 @@ from gbdxtools import Interface
 gbdx = Interface()
 
 
-data = 's3://receiving-dgcs-tdgplatform-com/054876618060_01_003'
+data = 's3://receiving-dgcs-tdgplatform-com/055605759010_01_003'
 
 aoptask = gbdx.Task("AOP_Strip_Processor", data=data, enable_acomp=True, enable_pansharpen=False, enable_dra=False, bands='MS')
 
 # Capture AOP task outputs
 #orthoed_output = aoptask.get_output('data')
 
-aop2envi = gbdx.Task("AOP_ENVI_HDR")
-aop2envi.inputs.image = aoptask.outputs.data.value
+envi_hdr = gbdx.Task("Build_ENVI_HDR")
+envi_hdr.inputs.image = aoptask.outputs.data.value
 
 #hdr file used to compute spectral index
 
 envi_ndvi = gbdx.Task("ENVI_SpectralIndex")
-envi_ndvi.inputs.input_raster = aop2envi.outputs.output_data.value
+envi_ndvi.inputs.input_raster = envi_hdr.outputs.output_data.value
 envi_ndvi.inputs.file_types = "hdr"
 envi_ndvi.inputs.index = "Normalized Difference Vegetation Index"
 
@@ -26,7 +26,7 @@ envi_color = gbdx.Task('ENVI_ColorSliceClassification', input_raster=envi_ndvi.o
 envi_color.file_types = 'hdr'
 
 
-workflow = gbdx.Workflow([aoptask, aop2envi, envi_ndvi, envi_color])
+workflow = gbdx.Workflow([aoptask, envi_hdr, envi_ndvi, envi_color])
 
 workflow.savedata(
   envi_ndvi.outputs.output_raster_uri,
